3b4eb260d4a270af5239729db9460f6aad38e427
Sander van den Akker <sander@myxt.nl>
2013-06-10 15:57:32 +0200

    Added Dutch translation.

---
+++ translations/dut-NL/translation.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>extension/nxc_social_networks</name>
+    <message>
+        <source>%secons seconds ago</source>
+        <translation>%secons seconden geleden</translation>
+    </message>
+    <message>
+        <source>%minutes minutes ago</source>
+        <translation>%minutes minuten geleden</translation>
+    </message>
+    <message>
+        <source>About %hours hours ago</source>
+        <translation>%hours uur geleden</translation>
+    </message>
+    <message>
+        <source>About %days days ago</source>
+        <translation>%days dagen geleden</translation>
+    </message>
+    <message>
+        <source>About %weeks weeks ago</source>
+        <translation>%weeks weken geleden</translation>
+    </message>
+</context>
+</TS>
