a7d024221674ba8e397511ccb6cbc9898a623c1e
Sergey Fedus <sf@ungvar.(none)>
2014-07-23 16:25:46 +0300

    replaced spaces with tabs

--- classes/feed/instagram.php
+++ classes/feed/instagram.php
@@ -14,10 +14,10 @@ class nxcSocialNetworksFeedInstagram extends nxcSocialNetworksFeed
 		$OAuth2Token = $OAuth2->getToken()->Token;
 
 		$this->API = array( 
-      'key'    => $OAuth2->appSettings[ 'key' ],
-      'secret' => $OAuth2->appSettings[ 'secret' ],
-      'token'  => $OAuth2Token
-    );
+			'key'    => $OAuth2->appSettings[ 'key' ],
+			'secret' => $OAuth2->appSettings[ 'secret' ],
+			'token'  => $OAuth2Token
+		);
 	}
 
 	public function getMediaRecent( $pageID = false, $limit = 20 ) {
@@ -38,49 +38,49 @@ class nxcSocialNetworksFeedInstagram extends nxcSocialNetworksFeed
 					self::$debugMessagesGroup
 				);
 
-        $items = array();
+				$items = array();
 
-        $userData = eZHTTPTool::getDataByURL(
-          'https://api.instagram.com/v1/users/search?' .
-          'q=' . $pageID . '&' .
-          'client_id=' . $this->API['key']
-        );
+				$userData = eZHTTPTool::getDataByURL(
+					'https://api.instagram.com/v1/users/search?' .
+					'q=' . $pageID . '&' .
+					'client_id=' . $this->API['key']
+				);
 
-        $userID = false;
+				$userID = false;
 
-        if( $userData !== false ) {
-          $userDataArray = json_decode( $userData, true );
-          if( isset( $userDataArray[ 'data' ][ 0 ] ) && $userDataArray[ 'data' ][ 0 ][ 'username' ] == $pageID ) {
-            $userID = $userDataArray[ 'data' ][ 0 ][ 'id' ];
-          }
-        }
+				if( $userData !== false ) {
+					$userDataArray = json_decode( $userData, true );
+					if( isset( $userDataArray[ 'data' ][ 0 ] ) && $userDataArray[ 'data' ][ 0 ][ 'username' ] == $pageID ) {
+						$userID = $userDataArray[ 'data' ][ 0 ][ 'id' ];
+					}
+				}
 
-        if( $userID !== false ) {
-          $leftLimit = $limit;
-          $feedData = eZHTTPTool::getDataByURL(
-            'https://api.instagram.com/v1/users/' .
-            $userID . '/media/recent/?' .
-            'access_token=' . $this->API[ 'token' ]
-          );
-          if( $feedData !== false ) {
-            $feedDataArray = json_decode( $feedData, true );
-            if( isset( $feedDataArray[ 'data' ] ) ) {
-              $items = array_merge( $items, array_slice( $feedDataArray[ 'data' ], 0, $leftLimit ) );
-              $leftLimit = $leftLimit - count( $feedDataArray[ 'data' ] );
-            }
-            $endlessLoopBreaker = 0;
-            while( $endlessLoopBreaker < 50 && $leftLimit > 0 && isset( $feedDataArray[ 'pagination' ][ 'next_url' ] ) ) {
-                $endlessLoopBreaker++;
-                $feedData = eZHTTPTool::getDataByURL( $feedDataArray[ 'pagination' ][ 'next_url' ] );
-                if( $feedData !== false ) {
-                  $feedDataArray = json_decode( $feedData, true );
-                  $items = array_merge( $items, array_slice( $feedDataArray[ 'data' ], 0, $leftLimit ) );
-                }
-                $leftLimit = $leftLimit - count( $feedDataArray[ 'data' ] );
+				if( $userID !== false ) {
+					$leftLimit = $limit;
+					$feedData = eZHTTPTool::getDataByURL(
+						'https://api.instagram.com/v1/users/' .
+						$userID . '/media/recent/?' .
+						'access_token=' . $this->API[ 'token' ]
+					);
+					if( $feedData !== false ) {
+						$feedDataArray = json_decode( $feedData, true );
+						if( isset( $feedDataArray[ 'data' ] ) ) {
+							$items = array_merge( $items, array_slice( $feedDataArray[ 'data' ], 0, $leftLimit ) );
+							$leftLimit = $leftLimit - count( $feedDataArray[ 'data' ] );
+						}
+						$endlessLoopBreaker = 0;
+						while( $endlessLoopBreaker < 50 && $leftLimit > 0 && isset( $feedDataArray[ 'pagination' ][ 'next_url' ] ) ) {
+								$endlessLoopBreaker++;
+								$feedData = eZHTTPTool::getDataByURL( $feedDataArray[ 'pagination' ][ 'next_url' ] );
+								if( $feedData !== false ) {
+								  $feedDataArray = json_decode( $feedData, true );
+								  $items = array_merge( $items, array_slice( $feedDataArray[ 'data' ], 0, $leftLimit ) );
+								}
+								$leftLimit = $leftLimit - count( $feedDataArray[ 'data' ] );
 
-            }
-          }
-        }
+						}
+					}
+				}
 				$cacheFileHandler->fileStoreContents( $cacheFileHandler->filePath, serialize( $items ) );
 			} else {
 				$items = unserialize( $cacheFileHandler->fetchContents() );
--- classes/handlers/oauth2/instagram.php
+++ classes/handlers/oauth2/instagram.php
@@ -13,7 +13,7 @@ class nxcSocialNetworksOAuth2Instagram extends nxcSocialNetworksOAuth2
 		return 'https://api.instagram.com/oauth/authorize/?' .
 			'client_id=' . $this->appSettings['key'] . '&' .
 			'redirect_uri=' . $redirectURL . '&' . 
-      'response_type=code';
+			'response_type=code';
 	}
 
 	public function getAccessToken( $redirectURL = null ) {
@@ -24,42 +24,42 @@ class nxcSocialNetworksOAuth2Instagram extends nxcSocialNetworksOAuth2
 		}
 		eZURI::transformURI( $redirectURL, false, 'full' );
 
-    $postdata = http_build_query(
-      array(
-        'client_id' => $this->appSettings['key'],
-        'grant_type' => 'authorization_code',
-        'client_secret' => $this->appSettings['secret'],
-        'code' => $http->getVariable( 'code' ),
-        'redirect_uri' => $redirectURL
-      )
-    );
-    $opts = array('http' =>
-      array(
-        'method'  => 'POST',
-        'content' => $postdata
-      )
-    );
-    $context = stream_context_create( $opts );
+		$postdata = http_build_query(
+			array(
+				'client_id' => $this->appSettings['key'],
+				'grant_type' => 'authorization_code',
+				'client_secret' => $this->appSettings['secret'],
+				'code' => $http->getVariable( 'code' ),
+				'redirect_uri' => $redirectURL
+			)
+		);
+		$opts = array('http' =>
+			array(
+				'method'  => 'POST',
+				'content' => $postdata
+			)
+		);
+		$context = stream_context_create( $opts );
 		$data = file_get_contents( 'https://api.instagram.com/oauth/access_token', false, $context );
-    $dataArray = json_decode( $data, true );
+		$dataArray = json_decode( $data, true );
 
-    if( isset( $adataArray[ 'access_token' ] ) ) {
-      return array(
-        'token'  => $adataArray[ 'access_token' ],
-        'secret' => null
-      );
-    }
+		if( isset( $adataArray[ 'access_token' ] ) ) {
+			return array(
+				'token'  => $adataArray[ 'access_token' ],
+				'secret' => null
+			);
+		}
 
-    if( isset( $dataArray[ 'access_token' ] ) ) {
-      return array(
-        'token'  => $dataArray[ 'access_token' ],
-        'secret' => null
-      );
-    }
+		if( isset( $dataArray[ 'access_token' ] ) ) {
+			return array(
+				'token'  => $dataArray[ 'access_token' ],
+				'secret' => null
+			);
+		}
 
-    if( isset( $dataArray[ 'error_message' ] ) && isset( $dataArray[ 'code' ] ) && isset( $dataArray[ 'error_type' ] ) ) {
-      throw new Exception( $dataArray[ 'error_type' ] . '(' . $dataArray[ 'code' ] . '): ' . $dataArray[ 'error_message' ] );
-    }
+		if( isset( $dataArray[ 'error_message' ] ) && isset( $dataArray[ 'code' ] ) && isset( $dataArray[ 'error_type' ] ) ) {
+		  throw new Exception( $dataArray[ 'error_type' ] . '(' . $dataArray[ 'code' ] . '): ' . $dataArray[ 'error_message' ] );
+		}
 
 		throw new Exception( 'Could not get access token. Refresh the page or try again later.' );
 	}
